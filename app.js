63e639f860c66fbda4d1c5828b8169e285535e9e
Rakshasss <gchanturidze24@gmail.com>
2023-08-10 20:31:38 +0400

    done

---
+++ app.js
@@ -0,0 +1,11 @@
+var Namewithvar = 100;
+let Nameiwithlet = "someone";
+const Namewithconst = "CARE";
+
+const myName = "Giorgi";
+const myHobby = "Programing";
+const yearOfBirth = 2004;
+const currentYear = 2023;
+
+
+console.log(`My name is ${myName}, I'm ${currentYear - yearOfBirth} years old and My hobby is ${myHobby} `);
